c583b98aecb10510d52afab9d992336501471b0c
NimnaKs <nimnakaveesha123456@gmail.com>
2023-12-26 10:38:50 +0530

    Add audio

--- js/script.js
+++ js/script.js
@@ -1,20 +1,20 @@
 /*=============== SHOW MENU ===============*/
 const navMenu = document.getElementById('nav-menu'),
-      navToggle = document.getElementById('nav-toggle'),
-      navClose = document.getElementById('nav-close')
+    navToggle = document.getElementById('nav-toggle'),
+    navClose = document.getElementById('nav-close')
 
 /*===== MENU SHOW =====*/
 /* Validate if constant exists */
-if(navToggle){
-    navToggle.addEventListener('click', () =>{
+if (navToggle) {
+    navToggle.addEventListener('click', () => {
         navMenu.classList.add('show-menu')
     })
 }
 
 /*===== MENU HIDDEN =====*/
 /* Validate if constant exists */
-if(navClose){
-    navClose.addEventListener('click', () =>{
+if (navClose) {
+    navClose.addEventListener('click', () => {
         navMenu.classList.remove('show-menu')
     })
 }
@@ -22,7 +22,7 @@ if(navClose){
 /*=============== REMOVE MENU MOBILE ===============*/
 const navLink = document.querySelectorAll('.nav__link')
 
-const linkAction = () =>{
+const linkAction = () => {
     const navMenu = document.getElementById('nav-menu')
     // When we click on each nav__link, we remove the show-menu class
     navMenu.classList.remove('show-menu')
@@ -30,41 +30,41 @@ const linkAction = () =>{
 navLink.forEach(n => n.addEventListener('click', linkAction))
 
 /*=============== CHANGE BACKGROUND HEADER ===============*/
-const scrollHeader = () =>{
+const scrollHeader = () => {
     const header = document.getElementById('header')
     // When the scroll is greater than 50 viewport height, add the scroll-header class to the header tag
-    this.scrollY >= 50 ? header.classList.add('bg-header') 
-                       : header.classList.remove('bg-header')
+    this.scrollY >= 50 ? header.classList.add('bg-header')
+        : header.classList.remove('bg-header')
 }
 window.addEventListener('scroll', scrollHeader)
 
-/*=============== SHOW SCROLL UP ===============*/ 
-const scrollUp = () =>{
-	const scrollUp = document.getElementById('scroll-up')
+/*=============== SHOW SCROLL UP ===============*/
+const scrollUp = () => {
+    const scrollUp = document.getElementById('scroll-up')
     // When the scroll is higher than 350 viewport height, add the show-scroll class to the a tag with the scrollup class
-	this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
-						: scrollUp.classList.remove('show-scroll')
+    this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
+        : scrollUp.classList.remove('show-scroll')
 }
 window.addEventListener('scroll', scrollUp)
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 const sections = document.querySelectorAll('section[id]')
-    
-const scrollActive = () =>{
-  	const scrollDown = window.scrollY
 
-	sections.forEach(current =>{
-		const sectionHeight = current.offsetHeight,
-			  sectionTop = current.offsetTop - 58,
-			  sectionId = current.getAttribute('id'),
-			  sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
+const scrollActive = () => {
+    const scrollDown = window.scrollY
 
-		if(scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight){
-			sectionsClass.classList.add('active-link')
-		}else{
-			sectionsClass.classList.remove('active-link')
-		}                                                    
-	})
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight,
+            sectionTop = current.offsetTop - 58,
+            sectionId = current.getAttribute('id'),
+            sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
+
+        if (scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight) {
+            sectionsClass.classList.add('active-link')
+        } else {
+            sectionsClass.classList.remove('active-link')
+        }
+    })
 }
 window.addEventListener('scroll', scrollActive)
 
@@ -72,14 +72,14 @@ window.addEventListener('scroll', scrollActive)
 let parallax = new Rellax('.parallax');
 
 /*=============== GSAP ANIMATION ===============*/
-gsap.from('.home__village', 1.2, {opacity: 0, y: 100, delay: .1})
-gsap.from('.home__pine', 1.2, {opacity: 0, y: 150, delay: .3})
-gsap.from('.home__mountain-2', 1.2, {opacity: 0, x: 150, delay: .5})
-gsap.from('.home__mountain-3', 1.2, {opacity: 0, x: -150, delay: .6})
-gsap.from('.home__mountain-1', 1.2, {opacity: 0, y: 250, delay: .7})
-gsap.from('.home__moon', 1.2, {opacity: 0, y: 200, delay: .8})
-gsap.from('.home__trineo', 1.2, {opacity: 0, x: -200, delay: .8})
-gsap.from('.home__title', 1.2, {opacity: 0, y: -60, delay: 1})
+gsap.from('.home__village', 1.2, { opacity: 0, y: 100, delay: .1 })
+gsap.from('.home__pine', 1.2, { opacity: 0, y: 150, delay: .3 })
+gsap.from('.home__mountain-2', 1.2, { opacity: 0, x: 150, delay: .5 })
+gsap.from('.home__mountain-3', 1.2, { opacity: 0, x: -150, delay: .6 })
+gsap.from('.home__mountain-1', 1.2, { opacity: 0, y: 250, delay: .7 })
+gsap.from('.home__moon', 1.2, { opacity: 0, y: 200, delay: .8 })
+gsap.from('.home__trineo', 1.2, { opacity: 0, x: -200, delay: .8 })
+gsap.from('.home__title', 1.2, { opacity: 0, y: -60, delay: 1 })
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
 const sr = ScrollReveal({
@@ -89,9 +89,9 @@ const sr = ScrollReveal({
     delay: 400
 })
 
-sr.reveal(`.about__data, .celebrate__img`, {origin: 'right'})
-sr.reveal(`.about__img, .celebrate__data`, {origin: 'left'})
-sr.reveal(`.send__card`, {interval: 100})
+sr.reveal(`.about__data, .celebrate__img`, { origin: 'right' })
+sr.reveal(`.about__img, .celebrate__data`, { origin: 'left' })
+sr.reveal(`.send__card`, { interval: 100 })
 sr.reveal(`.footer`)
 
 let snowfall = new Snowfall({
@@ -104,10 +104,17 @@ let snowfall = new Snowfall({
     minSpeed: 3,
     maxSpeed: 10,
     // custom symbol or text for snowflakes
-    text: "\u2744", 
+    text: "\u2744",
     // color of snowflakes
     color: "#ffffff",
     // z-index for the canvas
-    zIndex: "100000" 
+    zIndex: "100000"
 });
 
+// Use Howler.js to play audio on page load
+var sound = new Howl({
+    src: ['assets/audio/song.mp3'],
+    autoplay: true,
+    loop: true,
+    volume: 0.5, // Adjust the volume if needed
+});
